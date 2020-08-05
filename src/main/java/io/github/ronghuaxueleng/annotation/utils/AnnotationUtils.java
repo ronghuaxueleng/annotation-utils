60f6ba634e185150b6f7235598339b2c3b1f48e1
ronghuaxueleng <caoqianghappy@126.com>
2020-08-05 13:13:06 +0800

    首次提交

---
+++ src/main/java/io/github/ronghuaxueleng/annotation/utils/AnnotationUtils.java
@@ -0,0 +1,288 @@
+package io.github.ronghuaxueleng.annotation.utils;
+
+import io.github.ronghuaxueleng.annotation.entity.BeanAnnotationAttr;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.NotFoundException;
+import javassist.bytecode.*;
+import javassist.bytecode.annotation.Annotation;
+import javassist.bytecode.annotation.ArrayMemberValue;
+import javassist.bytecode.annotation.MemberValue;
+import javassist.bytecode.annotation.StringMemberValue;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * 注解中属性修改、查看工具
+ *
+ * @author: caoqiang
+ * @create: 2020/8/5 0005 下午 12:41
+ **/
+public class AnnotationUtils {
+
+    private static final Logger logger = Logger.getAnonymousLogger();
+
+    private final ClassUtils classUtils = new ClassUtils();
+
+    /**
+     * 在类上添加注解
+     *
+     * @param cc         当前类
+     * @param annoName   注解名
+     * @param fieldName  注解字段名
+     * @param fieldValue 注解字段值
+     */
+    public void addClassAnnotationFieldValue(CtClass cc, String annoName, String fieldName, Object fieldValue) {
+        AnnotationsAttribute attributeInfo = getClassAnnotation(cc);
+        // 添加新的注解
+        Annotation annotation = new Annotation(annoName, cc.getClassFile().getConstPool());
+        attributeInfo.addAnnotation(setAnnotation(cc.getClassFile().getConstPool(), annotation, fieldName, fieldValue));
+    }
+
+    /**
+     * 在类上添加注解
+     *
+     * @param cc       当前类
+     * @param annoName 注解名
+     * @param attrs    注解属性
+     */
+    public void addClassAnnotationFieldValue(CtClass cc, String annoName, List<BeanAnnotationAttr> attrs) {
+        AnnotationsAttribute attributeInfo = getClassAnnotation(cc);
+
+        // 添加新的注解
+        Annotation annotation = new Annotation(annoName, cc.getClassFile().getConstPool());
+        attributeInfo.addAnnotation(setAnnotation(cc.getClassFile().getConstPool(), annotation, attrs));
+    }
+
+    /**
+     * 获得类上的注解
+     *
+     * @param cc 类
+     * @return 注解对象
+     */
+    public AnnotationsAttribute getClassAnnotation(CtClass cc) {
+        ClassFile classFile = cc.getClassFile();
+        ConstPool constPool = classFile.getConstPool();
+
+        AnnotationsAttribute attributeInfo;
+        // 从这里取出原本类中的注解 建议DEBUG看下attributes中的数据
+        List<AttributeInfo> attributes = classFile.getAttributes();
+        if (attributes.size() > 1) {
+            attributeInfo = (AnnotationsAttribute) attributes.get(1);
+        } else {
+            attributeInfo = new AnnotationsAttribute(constPool, AnnotationsAttribute.visibleTag);
+            classFile.addAttribute(attributeInfo);
+        }
+        return attributeInfo;
+    }
+
+    /**
+     * 修改方法注解上的属性值
+     *
+     * @param className  当前类名
+     * @param methodName 当前方法名
+     * @param annoName   方法上的注解名
+     * @param fieldName  注解中的属性名
+     * @param fieldValue 注解中的属性值
+     */
+    public void setMethodAnnotatioinFieldValue(String className, String methodName, String annoName,
+                                               String fieldName, String fieldValue) throws NotFoundException {
+
+        CtClass cc = classUtils.createClassByClassName(className);
+        setMethodAnnotatioinFieldValue(cc, methodName, annoName, fieldName, fieldValue);
+    }
+
+    /**
+     * 修改方法注解上的属性值
+     *
+     * @param cc         当前类
+     * @param methodName 当前方法名
+     * @param annoName   方法上的注解名
+     * @param fieldName  注解中的属性名
+     * @param fieldValue 注解中的属性值
+     */
+    public void setMethodAnnotatioinFieldValue(CtClass cc, String methodName, String annoName, String fieldName, String fieldValue) throws NotFoundException {
+        classUtils.defrost(cc);
+        MethodInfo methodInfo = classUtils.getMethod(cc, methodName);
+        ConstPool constPool = methodInfo.getConstPool();
+        AnnotationsAttribute attr = (AnnotationsAttribute) methodInfo.getAttribute(AnnotationsAttribute.visibleTag);
+        Annotation annotation = attr.getAnnotation(annoName);
+        if (annotation != null) {
+            annotation.addMemberValue(fieldName, new StringMemberValue(fieldValue, constPool));
+            attr.setAnnotation(annotation);
+            methodInfo.addAttribute(attr);
+        }
+    }
+
+    /**
+     * 给方法添加注解
+     *
+     * @param className  类名
+     * @param methodName 方法名
+     * @param annoName   注解名
+     * @param fieldName  字段名
+     * @param fieldValue 字段值
+     */
+    public void addMethodAnnotatioinFieldValue(String className, String methodName, String annoName, String fieldName, String fieldValue) throws NotFoundException {
+        CtClass cc = classUtils.createClassByClassName(className);
+        addMethodAnnotatioinFieldValue(cc, methodName, annoName, fieldName, fieldValue);
+    }
+
+    /**
+     * 给方法添加注解
+     *
+     * @param cc         当前类
+     * @param methodName 方法名
+     * @param annoName   注解名
+     * @param fieldName  字段名
+     * @param fieldValue 字段值
+     */
+    public void addMethodAnnotatioinFieldValue(CtClass cc, String methodName, String annoName, String fieldName, String fieldValue) throws NotFoundException {
+        classUtils.defrost(cc);
+        MethodInfo methodInfo = classUtils.getMethod(cc, methodName);
+        ConstPool constPool = methodInfo.getConstPool();
+        addMethodAnnotatioinFieldValue(cc, methodName, annoName, fieldName, new StringMemberValue(fieldValue, constPool));
+    }
+
+    public void addMethodAnnotatioinFieldValue(CtClass cc, String methodName, String annoName, List<BeanAnnotationAttr> attrs) {
+        try {
+            classUtils.defrost(cc);
+            MethodInfo methodInfo = classUtils.getMethod(cc, methodName);
+            ConstPool constPool = methodInfo.getConstPool();
+            AnnotationsAttribute attr = (AnnotationsAttribute) methodInfo.getAttribute(AnnotationsAttribute.visibleTag);
+            if (attr == null) {
+                attr = new AnnotationsAttribute(constPool, AnnotationsAttribute.visibleTag);
+            }
+            Annotation annotation = new Annotation(annoName, constPool);
+            attr.addAnnotation(setAnnotation(constPool, annotation, attrs));
+            methodInfo.addAttribute(attr);
+        } catch (NotFoundException e) {
+            logger.info(e.getMessage());
+        }
+    }
+
+    /**
+     * 给方法添加注解
+     *
+     * @param cc         当前类
+     * @param methodName 方法名
+     * @param annoName   注解名
+     * @param fieldName  注解属性字段
+     * @param fieldValue 注解属性值
+     */
+    public void addMethodAnnotatioinFieldValue(CtClass cc, String methodName, String annoName, String fieldName,
+                                               Object fieldValue) throws NotFoundException {
+        classUtils.defrost(cc);
+        MethodInfo methodInfo = classUtils.getMethod(cc, methodName);
+        ConstPool constPool = methodInfo.getConstPool();
+        AnnotationsAttribute attr = (AnnotationsAttribute) methodInfo.getAttribute(AnnotationsAttribute.visibleTag);
+        Annotation annotation = new Annotation(annoName, constPool);
+        attr.addAnnotation(setAnnotation(constPool, annotation, fieldName, fieldValue));
+        methodInfo.addAttribute(attr);
+    }
+
+    /**
+     * 给方法添加注解
+     *
+     * @param cc         当前类
+     * @param methodName 方法名
+     * @param annoName   注解名
+     * @param fieldName  字段名
+     * @param fieldValue 字段值
+     */
+    public void addMethodAnnotatioinFieldValue(CtClass cc, String methodName, String annoName,
+                                               String fieldName, MemberValue fieldValue) throws NotFoundException {
+        classUtils.defrost(cc);
+        MethodInfo methodInfo = classUtils.getMethod(cc, methodName);
+        ConstPool constPool = methodInfo.getConstPool();
+        AnnotationsAttribute attr = (AnnotationsAttribute) methodInfo.getAttribute(AnnotationsAttribute.visibleTag);
+        Annotation annotation = new Annotation(annoName, constPool);
+        annotation.addMemberValue(fieldName, fieldValue);
+        attr.addAnnotation(annotation);
+        methodInfo.addAttribute(attr);
+    }
+
+    /**
+     * 设置注解
+     *
+     * @param constPool  类
+     * @param annotation 注解
+     * @param attrs      属性
+     */
+    public Annotation setAnnotation(ConstPool constPool, Annotation annotation, List<BeanAnnotationAttr> attrs) {
+        for (BeanAnnotationAttr attr : attrs) {
+            String fieldName = attr.getAttrName();
+            Object fieldValue = attr.getAttrValue();
+            setAnnotation(constPool, annotation, fieldName, fieldValue);
+        }
+        return annotation;
+    }
+
+    /**
+     * @param constPool  类
+     * @param annotation 注解
+     * @param fieldName  字段名
+     * @param fieldValue 字段值
+     */
+    public Annotation setAnnotation(ConstPool constPool, Annotation annotation, String fieldName, Object fieldValue) {
+        if (fieldValue instanceof String) {
+            annotation.addMemberValue(fieldName, new StringMemberValue(fieldValue.toString(), constPool));
+        } else if (fieldValue instanceof String[]) {
+            String[] objects = (String[]) fieldValue;
+            ArrayMemberValue memberValue = new ArrayMemberValue(constPool);
+            StringMemberValue[] stringMemberValues = new StringMemberValue[objects.length];
+            for (int i = 0; i < objects.length; i++) {
+                stringMemberValues[i] = new StringMemberValue(objects[i], constPool);
+            }
+            memberValue.setValue(stringMemberValues);
+            annotation.addMemberValue(fieldName, memberValue);
+        } else if (fieldValue instanceof List) {
+            List<String> objects = (List<String>) fieldValue;
+            ArrayMemberValue memberValue = new ArrayMemberValue(constPool);
+            StringMemberValue[] stringMemberValues = new StringMemberValue[objects.size()];
+            for (int i = 0; i < objects.size(); i++) {
+                stringMemberValues[i] = new StringMemberValue(objects.get(i), constPool);
+            }
+            memberValue.setValue(stringMemberValues);
+            annotation.addMemberValue(fieldName, memberValue);
+        }
+        return annotation;
+    }
+
+    /**
+     * 获取方法注解中的属性值
+     *
+     * @param className  当前类名
+     * @param methodName 当前方法名
+     * @param annoName   方法上的注解名
+     * @param fieldName  注解中的属性名
+     */
+    public String getMethodAnnotatioinFieldValue(String className, String methodName, String annoName, String fieldName) throws NotFoundException {
+        CtClass cc = classUtils.createClassByClassName(className);
+        return getMethodAnnotatioinFieldValue(cc, methodName, annoName, fieldName);
+    }
+
+    /**
+     * 获取方法注解中的属性值
+     *
+     * @param cc         当前类
+     * @param methodName 当前方法名
+     * @param annoName   方法上的注解名
+     * @param fieldName  注解中的属性名
+     */
+    public String getMethodAnnotatioinFieldValue(CtClass cc, String methodName, String annoName, String fieldName) throws NotFoundException {
+        classUtils.defrost(cc);
+        MethodInfo methodInfo = classUtils.getMethod(cc, methodName);
+        AnnotationsAttribute attr = (AnnotationsAttribute) methodInfo.getAttribute(AnnotationsAttribute.visibleTag);
+        String value = "";
+        if (attr != null) {
+            Annotation an = attr.getAnnotation(annoName);
+            if (an != null) {
+                value = ((StringMemberValue) an.getMemberValue(fieldName)).getValue();
+            }
+        }
+        return value;
+    }
+}
---
+++ src/main/java/io/github/ronghuaxueleng/annotation/utils/ClassUtils.java
@@ -0,0 +1,93 @@
+package io.github.ronghuaxueleng.annotation.utils;
+
+import io.github.ronghuaxueleng.annotation.entity.ReturnTypeField;
+import javassist.*;
+import javassist.bytecode.MethodInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * 类操作工具
+ *
+ * @author: caoqiang
+ * @create: 2020/8/5 0005 下午 12:41
+ **/
+public class ClassUtils {
+
+    private static final Logger logger = Logger.getAnonymousLogger();
+
+    /**
+     * 解冻
+     *
+     * @param cc 当前类
+     */
+    public void defrost(CtClass cc) {
+        if (cc.isFrozen()) {
+            cc.defrost();
+        }
+    }
+
+    /**
+     * 通过类名生成class
+     *
+     * @param className 类名
+     * @return 类对象
+     * @throws NotFoundException 类没有找到异常
+     */
+    public CtClass createClassByClassName(String className) throws NotFoundException {
+        ClassPool classPool = ClassPool.getDefault();
+        return classPool.get(className);
+    }
+
+    /**
+     * 通过方法名获得方法
+     *
+     * @param cc         当前类
+     * @param methodName 方法名
+     * @return 方法对象
+     * @throws NotFoundException 方法没有找到
+     */
+    public MethodInfo getMethod(CtClass cc, String methodName) throws NotFoundException {
+        CtMethod ctMethod = cc.getDeclaredMethod(methodName);
+        return ctMethod.getMethodInfo();
+    }
+
+    /**
+     * 获得类字段
+     *
+     * @param className 类名
+     * @return
+     */
+    public List<ReturnTypeField> getClassFields(String className) {
+        List<ReturnTypeField> returnTypeFieldList = new ArrayList<>();
+        try {
+            // 创建类
+            CtClass cc = createClassByClassName(className);
+            //获得所有属性
+            CtField[] declaredFields = cc.getDeclaredFields();
+            for (CtField declaredField : declaredFields) {
+                // 获取属性的名字
+                String name = declaredField.getName();
+                // 将属性的首字符大写，方便构造get，set方法
+                name = name.substring(0, 1).toUpperCase() + name.substring(1);
+                try {
+                    CtMethod declaredMethod = cc.getDeclaredMethod("get" + name);
+                    if (declaredMethod != null) {
+                        ReturnTypeField returnTypeField = new ReturnTypeField();
+                        returnTypeField.setGenericType(declaredField.getType().getName());
+                        returnTypeField.setName(declaredField.getName());
+                        returnTypeFieldList.add(returnTypeField);
+                    }
+                } catch (NotFoundException e) {
+                    logger.info(e.getMessage());
+                }
+            }
+        } catch (NotFoundException e) {
+            logger.info(e.getMessage());
+        }
+        return returnTypeFieldList;
+    }
+
+}
